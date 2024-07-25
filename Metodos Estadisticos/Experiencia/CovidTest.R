316a82452fc6a51912fb9222ed85d2919797d435
JuanjoRestrepo <restrepojuanjo@gmail.com>
2024-07-24 21:31:09 -0500

    Folders Retos y Metodos

---
+++ Metodos Estadisticos/Experiencia/CovidTest.R
@@ -0,0 +1,4 @@
+library(RSocrata)    # llamado de libreria
+token <- "zxMsD6eXc0zlEMryRGW87Hwrz"  # token
+Colombia <- read.socrata("https://www.datos.gov.co/resource/gt2j-8ykr.json", app_token = token) # lectura 
+saveRDS(Colombia, file = "data/Colombia.RDS") 
