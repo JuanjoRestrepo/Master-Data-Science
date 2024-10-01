ff4ec97bdc95a3afc5ad402579ca9f151f26c3c0
JuanjoRestrepo <restrepojuanjo@gmail.com>
2024-10-01 18:48:05 -0500

    Query E

---
+++ Gestion Datos/Actividad 2/Consultas/Query E.sql
@@ -0,0 +1,16 @@
+-- Query E. 
+-- Listar los estudiantes que se han inscrito a un curso determinado. 
+-- Se deben obtener los siguientes datos: el nombre completo del estudiante y el nombre del curso.
+
+SELECT distinct
+   cc.id_curso,
+    c.nombre AS nombre_curso,
+    u.nombre || ' ' || u.apellido AS nombre_estudiante
+FROM
+    USUARIOS u
+INNER JOIN Cursos_Estudiantes ce ON u.id_usuario = ce.id_usuario
+INNER JOIN Calendario_Cursos cc ON ce.id_calendario = cc.id_calendario
+INNER JOIN Cursos c ON cc.id_curso = c.id_curso
+WHERE
+    c.id_curso= :idcurso
+    order by nombre_estudiante;
