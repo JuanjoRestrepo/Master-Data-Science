8764ecac563c479d91da2d90991f240780091f9e
Juan José Restrepo Rosero <49797614+JuanjoRestrepo@users.noreply.github.com>
2024-10-01 15:54:04 -0500

    Add files via upload

---
+++ Gestion Datos/Actividad 2/Consultas/Consulta_B.sql
@@ -0,0 +1,22 @@
+--Query B: 
+-- Obtener la lista de profesores que dictan cursos pertenecientes a la
+-- facultad de Humanidades. Se deben obtener los siguientes datos: el
+-- nombre completo del profesor y el nombre del curso que dicta..
+
+SELECT 
+    fac.nombre AS facultad_nombre,
+    curso.nombre AS curso_nombre,
+    prof.nombre || ' ' || prof.apellido AS profesor_completo
+FROM
+    Usuarios prof
+INNER JOIN 
+    Calendario_Cursos calendario ON prof.id_usuario = calendario.id_profesor
+INNER JOIN 
+    Cursos curso ON calendario.id_curso = curso.id_curso
+INNER JOIN 
+    Facultades fac ON curso.id_facultad = fac.id_facultad
+WHERE
+    fac.nombre LIKE '%Humanidades%'  -- Aquí usamos el nombre para mayor legibilidad
+ORDER BY
+    prof.nombre, prof.apellido, curso.nombre;
+
